95484bdf64efb94a4afec001d7c46dffcc9569ae
jason harris <jason@overhq.com>
2020-10-20 17:12:57 +0200

    An 'excludes' file with whitspace and another without

---
+++ src/shouldAlsoFail.js
@@ -0,0 +1,20 @@
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/ba0d86b4a03f476b450cdf7d3057be62
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/ba0d86b4a03f476b450cdf7d3057be62
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
